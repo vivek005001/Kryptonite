eafd59f699c8ce4111206bb106b590831daf5553
Satyam Kumar <b22236@students.iitmandi.ac.in>
2026-02-15 18:10:57 +0530

    remove next app

--- kryptonite-web/backend/test_api.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://cool-starfish-suitable.ngrok-free.app/analyze"
-files = {'file': open('/home/teaching/Kryptonite/apps/DVIA-v2-swift.ipa', 'rb')}
-response = requests.post(url, files=files)
-print("Status Code:", response.status_code)
-print("Response:", response.json())
